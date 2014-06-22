0c86ac255f3fada2032e0977c3d708bdf871cbd6
mihayloff <mihayloff14@gmail.com>
2014-06-22 11:17:26 +0300

    added console Games

---
+++ ConsoleGames/JustCars-Niki/JustCars.cs
@@ -0,0 +1,176 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+struct Object
+{
+    public int x;
+    public int y;
+    public char c;
+    public ConsoleColor color;
+}
+
+class JustCars
+{
+    static void PrintOnPosition(int x, int y, char c,
+        ConsoleColor color = ConsoleColor.Gray)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.ForegroundColor = color;
+        Console.Write(c);
+    }
+    static void PrintStringOnPosition(int x, int y, string str,
+        ConsoleColor color = ConsoleColor.Gray)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.ForegroundColor = color;
+        Console.Write(str);
+    }
+
+    static void Main()
+    {
+        double speed = 100.0;
+        int playfieldWidth = 5;
+        int livesCount = 5;
+        Console.BufferHeight = Console.WindowHeight = 20;
+        Console.BufferWidth = Console.WindowWidth = 30;
+
+        Object userCar = new Object();
+        userCar.x = 2;
+        userCar.y = Console.WindowHeight - 1;
+        userCar.c = '@';
+        userCar.color = ConsoleColor.Yellow;
+        Random randomGenerator = new Random();
+        List<Object> objects = new List<Object>();
+
+        while (true)
+        {
+            bool carHit = false;
+            {
+                int chance = randomGenerator.Next(0, 100);
+                if (chance < 10)
+                {
+                    Object NewObject = new Object();
+                    NewObject.color = ConsoleColor.Blue;
+                    NewObject.c = '$';
+                    NewObject.x = randomGenerator.Next(0, playfieldWidth);
+                    NewObject.y = 0;
+                    objects.Add(NewObject);
+                }
+                else if (chance < 20)
+                {
+                    Object NewObject = new Object();
+                    NewObject.color = ConsoleColor.Magenta;
+                    NewObject.c = '*';
+                    NewObject.x = randomGenerator.Next(0, playfieldWidth);
+                    NewObject.y = 0;
+                    objects.Add(NewObject);
+                }
+                else
+                {
+
+
+                    Object newCar = new Object();
+                    newCar.color = ConsoleColor.Green;
+                    newCar.x = randomGenerator.Next(0, playfieldWidth);
+                    newCar.y = 0;
+                    newCar.c = '#';
+                    objects.Add(newCar);
+                }
+            }
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                while (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true);
+                }
+                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                {
+                    if (userCar.x - 1 >= 0)
+                    {
+                        userCar.x = userCar.x - 1;
+                    }
+
+                }
+                else if (pressedKey.Key == ConsoleKey.RightArrow)
+                {
+                    if (userCar.x + 1 < playfieldWidth)
+                    {
+                        userCar.x = userCar.x + 1;
+                    }
+                }
+            }
+            List<Object> newList = new List<Object>();
+            for (int i = 0; i < objects.Count; i++)
+            {
+                Object oldCar = objects[i];
+                Object newObject = new Object();
+                newObject.x = oldCar.x;
+                newObject.y = oldCar.y + 1;
+                newObject.c = oldCar.c;
+                newObject.color = oldCar.color;
+                if (newObject.c == '$' && newObject.y == userCar.y && newObject.x == userCar.x)
+                {
+                    objects.Clear();
+                }
+                if (newObject.c == '*' && newObject.y == userCar.y && newObject.x == userCar.x)
+                {
+                    speed -= 20;
+                }
+                if (newObject.c == '#' && newObject.y == userCar.y && newObject.x == userCar.x)
+                {
+                    carHit = true;
+                    livesCount--;
+                    speed += 50;
+                    if (speed > 400)
+                    {
+                        speed = 400;
+                    }
+                    if (livesCount <= 0)
+                    {
+                        PrintStringOnPosition(8, 10, "GAME OVER!", ConsoleColor.Red);
+                        PrintStringOnPosition(8, 12, "Press [enter] to exit", ConsoleColor.Red);
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                if (newObject.y < Console.WindowHeight)
+                {
+                    newList.Add(newObject);
+                }
+            }
+            objects = newList;
+            Console.Clear();
+            foreach (Object car in objects)
+            {
+                PrintOnPosition(car.x, car.y, car.c, car.color);
+            }
+            PrintOnPosition(userCar.x, userCar.y, userCar.c, userCar.color);
+            if (carHit == true)
+            {
+                objects.Clear();
+                PrintOnPosition(userCar.x, userCar.y, 'X', ConsoleColor.Red);
+            }
+            // Draw info
+            PrintStringOnPosition(8, 4, "Lives: " + livesCount, ConsoleColor.White);
+            PrintStringOnPosition(8, 5, "Speed: " + (int)speed, ConsoleColor.Cyan);
+
+            speed += 0.5;
+            if (speed > 400)
+            {
+                speed = 400;
+            }
+
+            Thread.Sleep((int)(600 - speed));
+
+
+
+        }
+    }
+}
+
---
+++ ConsoleGames/TronGame/TronGame.cs
@@ -0,0 +1,247 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+	
+struct Position
+{
+	public int x;
+	public int y;
+	public Position(int x, int y)
+	{
+	    this.x=x;
+	    this.y=y;
+	}
+}
+class TronGame
+{
+	// values
+	static int playField = 3;
+	
+	static int down = 0;
+	static int up = 1;
+	static int right = 2;
+	static int left = 3;
+	
+	static byte scoreP1 = 0;
+	static byte scoreP2 = 0;
+	
+	static int directionP1 = right;
+	static int directionP2 = left;
+	
+	static Queue<Position> player1 = new Queue<Position>();
+	static Queue<Position> player2 = new Queue<Position>();
+	static Position[] directions = new Position[]
+	    {
+	        new Position(0, 1), // down
+	        new Position(0,-1), // up
+	        new Position(1,0), // right
+	        new Position(-1,0),  // left
+	    };
+	
+	static Position player1newhead = new Position();
+	static Position player2newhead = new Position();
+	
+	static Position player1head = new Position();
+	static Position NextDirectionP1 = new Position();
+	
+	static Position player2head = new Position();
+	static Position NextDirectionP2 = new Position();
+	
+	static void PrintStringOnPosition(int col, int row, string str,
+	    ConsoleColor color = ConsoleColor.Gray)
+	{
+	    Console.SetCursorPosition(col, row);
+	    Console.ForegroundColor = color;
+	    Console.Write(str);
+	}
+	
+	static void SetGameField()
+	{
+	    Console.CursorVisible = false;
+	    Console.BufferHeight = Console.WindowHeight = 35;
+	    Console.BufferWidth = Console.WindowWidth = 80;
+	    for (int i = 0; i < Console.BufferWidth; i++)
+	    {
+	        PrintStringOnPosition(i, playField, "_", ConsoleColor.White);
+	    }
+	
+	    PrintStringOnPosition(7, 1, "Player 1:", ConsoleColor.Magenta);
+	    PrintStringOnPosition(56, 1, "Player 2:", ConsoleColor.Magenta);
+	
+	    for (int i = 0; i < 4; i++)
+	    {
+	        player1.Enqueue(new Position(i, 15));
+	    }
+	
+	    foreach (Position element in player1)
+	    {
+	        PrintStringOnPosition(element.x, element.y, "*", ConsoleColor.Green);
+	    }
+	
+	    for (int i = 1; i < 5; i++)
+	    {
+	        player2.Enqueue(new Position(Console.BufferWidth - i, 15));
+	    }
+	    foreach (Position element in player2)
+	    {
+	        PrintStringOnPosition(element.x, element.y, "*", ConsoleColor.Yellow);
+	
+	    }
+	    PlayerMovement();
+	}
+	
+	static void ChangePlayersDirection(ConsoleKeyInfo playersInput)
+	{
+	    //player 1
+	    if (playersInput.Key == ConsoleKey.A && directionP1 != right)
+	    {
+	        directionP1 = left;
+	    }
+	    if (playersInput.Key == ConsoleKey.D && directionP1 != left)
+	    {
+	        directionP1 = right;
+	    }
+	    if (playersInput.Key == ConsoleKey.S && directionP1 != up)
+	    {
+	        directionP1 = down;
+	    }
+	    if (playersInput.Key == ConsoleKey.W && directionP1 != down)
+	    {
+	        directionP1 = up;
+	    }
+	
+	    //player 2
+	    if (playersInput.Key == ConsoleKey.LeftArrow && directionP2 != right)
+	    {
+	        directionP2 = left;
+	    }
+	    if (playersInput.Key == ConsoleKey.RightArrow && directionP2 != left)
+	    {
+	        directionP2 = right;
+	    }
+	    if (playersInput.Key == ConsoleKey.DownArrow && directionP2 != up)
+	    {
+	        directionP2 = down;
+	    }
+	    if (playersInput.Key == ConsoleKey.UpArrow && directionP2 != down)
+	    {
+	        directionP2 = up;
+	    }
+	}
+	
+	static void PlayerMovement()
+	{
+	    player1head = player1.Last();
+	    NextDirectionP1 = directions[directionP1];
+	    player1newhead = new Position(player1head.x + NextDirectionP1.x, player1head.y + NextDirectionP1.y);
+	
+	    player2head = player2.Last();
+	    NextDirectionP2 = directions[directionP2];
+	    player2newhead = new Position(player2head.x + NextDirectionP2.x, player2head.y + NextDirectionP2.y);
+	}
+	
+	static void ResetGame()
+	{
+	    foreach (Position element in player1)
+	    {
+	        PrintStringOnPosition(element.x, element.y, " ", ConsoleColor.Green);
+	    }
+	    foreach (Position element in player2)
+	    {
+	        PrintStringOnPosition(element.x, element.y, " ", ConsoleColor.Yellow);
+	    }
+	    player1.Clear();
+	    player2.Clear();
+	    directionP1 = right;
+	    directionP2 = left;
+	    PrintStringOnPosition(30, 0, @"             ", ConsoleColor.White);
+	    SetGameField();
+	}
+	
+	static void GameOver()
+	{
+	    PrintStringOnPosition(15, 1, "" + (int)scoreP1, ConsoleColor.Cyan);
+	    PrintStringOnPosition(67, 1, "" + (int)scoreP2, ConsoleColor.Cyan);
+	    PrintStringOnPosition(30, 0, "GAME OVER!", ConsoleColor.Red);
+	    PrintStringOnPosition(30, 1, "Player 1 Wins", ConsoleColor.Red);
+	    PrintStringOnPosition(30, 2, "the game!", ConsoleColor.Red);
+	    Console.ReadKey(true);
+	    return;
+	}
+	
+	static void EndCurrentGame()
+	{
+	    if (player1.Contains(player2newhead) && player2.Contains(player1newhead))
+	    {
+	        PrintStringOnPosition(player1head.x, player1head.y, "O", ConsoleColor.Blue);
+	        PrintStringOnPosition(37, 0, "Draw!", ConsoleColor.Red);
+	        Console.ReadKey(true);
+	        ResetGame();
+	    }
+	
+	    if (player2.Contains(player2newhead) ||
+	            player1.Contains(player2newhead) ||
+	            player2newhead.x < 0 || player2newhead.x > Console.WindowWidth - 1 ||
+	            player2newhead.y <= 3 || player2newhead.y > Console.WindowHeight - 1)
+	    {
+	        scoreP1++;
+	        if (scoreP1 == 5)
+	        {
+	            GameOver();
+	        }
+	        PrintStringOnPosition(30, 0, "Player 1 Wins", ConsoleColor.Red);
+	        Console.ReadKey();
+	        ResetGame();
+	    }
+	
+	    if (player1.Contains(player1newhead) ||
+	        player2.Contains(player1newhead) ||
+	        player1newhead.x < 0 || player1newhead.x > Console.WindowWidth - 1 ||
+	        player1newhead.y <= 3 || player1newhead.y > Console.WindowHeight - 1)
+	    {
+	        scoreP2++;
+	        if (scoreP2 == 5)
+	        {
+	            GameOver();
+	        }
+	        PrintStringOnPosition(30, 0, "Player 2 Wins", ConsoleColor.Red);
+	        Console.ReadKey(true);
+	        ResetGame();
+	    }
+	}
+	static void Main()
+	{
+	    SetGameField();
+	
+	    while (true)
+	    {
+	        if (Console.KeyAvailable)
+	        {
+	            ConsoleKeyInfo playersInput = Console.ReadKey(true);
+	            ChangePlayersDirection(playersInput);
+	
+	        }
+	
+	        PlayerMovement();
+	
+	        EndCurrentGame();
+	
+	        player1.Enqueue(player1newhead);
+	        player2.Enqueue(player2newhead);
+	
+	        PrintStringOnPosition(player1newhead.x, player1newhead.y, "*", ConsoleColor.Green);
+	        PrintStringOnPosition(player2newhead.x, player2newhead.y, "*", ConsoleColor.Yellow);
+	                
+	        PrintStringOnPosition(17, 1, "" + (int)scoreP1, ConsoleColor.Cyan);
+	        PrintStringOnPosition(66, 1, "" + (int)scoreP2, ConsoleColor.Cyan);
+	        Thread.Sleep(100);
+	    }
+	
+	}
+	
+}
+	
+
