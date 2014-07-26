1a3c5b5c5758cfb63f0c8deaadf1c7af8f3a49c5
mihayloff <mihayloff14@gmail.com>
2014-07-26 14:26:29 +0300

    added exam from 25 july evening

---
+++ Exam Preparation/Software University exams/Exam25.07.2014-evening/05.ChangeEvenBits/ChangeEvenBits.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class ChangeEvenBits
+{
+    static void Main()
+    {
+        int numOfInputs = int.Parse(Console.ReadLine());
+        long[] nums = new long[numOfInputs];
+
+        for (int index = 0; index < numOfInputs; index++)
+        {
+            long number = long.Parse(Console.ReadLine());
+            nums[index] = number;
+        }
+
+        long numToChange = long.Parse(Console.ReadLine());
+        long changesCount = 0;
+
+        for (int index = 0; index < numOfInputs; index++)
+        {
+            int count = 0;
+
+            if (nums[index] == 0)
+            {
+                count = 1;
+            }
+
+            while (nums[index] > 0)
+            {
+                count++;
+                nums[index] >>= 1;
+            }
+
+            for (int bit = 0; bit < 64; bit++)
+            {
+                if (bit % 2 == 0)
+                {
+                    count--;
+                    long checkBit = (numToChange >> bit) & 1;
+                    if (checkBit == 0)
+                    {
+                        numToChange ^= (long)1 << bit;
+                        changesCount++;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(numToChange);
+        Console.WriteLine(changesCount);
+    }
+}
---
+++ Exam Preparation/Software University exams/Exam25.07.2014-evening/AuthorSolutions/HouseWithWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class HouseWithWindow
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int starsBefore = n - 1;
+        int starsMiddle = 1;
+
+        Console.WriteLine("{0}*{0}", new string('.', starsBefore));
+        for (int i = 0; i < n - 1; i++)
+        {
+            starsBefore--;
+            Console.WriteLine("{0}*{1}*{0}",
+                new String('.', starsBefore), new String('.', starsMiddle));
+            starsMiddle += 2;
+        }
+
+        Console.WriteLine(new string('*', 2 * n - 1));
+        for (int i = 0; i < n / 4; i++)
+        {
+            Console.WriteLine("*{0}*", new string('.', n * 2 - 3));
+        }
+
+        int dotsBetweenWindows = n / 2;
+        int windowsStars = n - 3;
+        for (int i = 0; i < n / 2; i++)
+        {
+            Console.WriteLine("*{0}{1}{0}*",
+                new string('.', dotsBetweenWindows), new string('*', windowsStars));
+        }
+
+        for (int i = 0; i < n / 4; i++)
+        {
+            Console.WriteLine("*{0}*", new string('.', n * 2 - 3));
+        }
+        Console.WriteLine(new string('*', 2 * n - 1));
+    }
+}
