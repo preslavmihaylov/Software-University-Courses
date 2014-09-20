1f49a3fc6ba83d306f4cf9cda08e3c47373edc9f
Preslav Mihaylov <mihayloff14@gmail.com>
2014-09-20 11:06:54 +0300

    created a new regex for finding emails.

--- 04.JavaCollectionsBasics/src/_08_ExtractEmails.java
+++ 04.JavaCollectionsBasics/src/_08_ExtractEmails.java
@@ -11,13 +11,15 @@ public class _08_ExtractEmails {
 		String[] input = scan.nextLine().split(" ");
 		String temp = "";
 		
+		// in order to remove the dot at the end of each text
 		for (int index = 0; index < input[input.length - 1].length() - 1; index++) {
 			temp += input[input.length - 1].charAt(index);
 		}
 		
 		input[input.length - 1] = temp;
 		
-		String regex = "([a-zA-Z0-9]+[.,-]?[a-zA-Z0-9]+)+[@]{1}([a-zA-Z0-9]+[.-]?[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]+)+";
+		String regex = "([a-zA-Z0-9]+[.,-_]?[a-zA-Z0-9]+)+[@]{1}"
+				+ "([a-zA-Z0-9]+[.-]?[a-zA-Z0-9]+)+[.]{1}[a-zA-Z0-9]+";
 		
 		for (String word : input) {
 			if (word.matches(regex)) {
