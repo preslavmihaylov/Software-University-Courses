4ae27473996d7f191f9bcc4d5899cfddb1581bd3
mihayloff <mihayloff14@gmail.com>
2014-11-03 15:24:19 +0200

    added some of the tasks from the first homework

---
+++ JavaScript Basics/01. Introduction to JavaScript/04. Circle Area/04.CircleArea.js
@@ -0,0 +1,4 @@
+document.getElementById("first").innerHTML += Math.PI * Math.pow(7, 2);
+document.getElementById("second").innerHTML += Math.PI * Math.pow(1.5, 2);
+document.getElementById("third").innerHTML += Math.PI * Math.pow(20, 2);
+
---
+++ JavaScript Basics/01. Introduction to JavaScript/05. DecToHex/05.DecToHex.js
@@ -0,0 +1,5 @@
+var input = prompt("Please enter a number", "200");
+
+var input = parseInt(input).toString(16).toUpperCase();
+
+window.alert(input);
