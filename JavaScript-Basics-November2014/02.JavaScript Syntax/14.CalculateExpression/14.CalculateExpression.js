9081b1c4a9ba0a158fec58555b09e4d446361848
mihayloff <mihayloff14@gmail.com>
2014-11-08 23:27:36 +0200

    added the final task from the second homework

---
+++ JavaScript-Basics-November2014/02.JavaScript Syntax/14.CalculateExpression/14.CalculateExpression.js
@@ -0,0 +1,8 @@
+﻿function evalResult() {
+    var expression = document.getElementById('input').value;
+
+    var result = document.getElementById('output');
+    result.innerHTML = eval(expression);
+}
+
+// I am sick of HTML/CSS bullshit s I'm not bothering styling that html file
