d7d05431d723232cc441f54435ebef795d312ea8
mihayloff <mihayloff14@gmail.com>
2014-11-11 13:51:02 +0200

    added the refactored folder

---
+++ JavaScript-Basics-November2014/02.JavaScript Syntax/02.SimpleCalculations/02.SimpleCalculations.js
@@ -0,0 +1,17 @@
+﻿var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Input a number: ", function (answer) {
+    
+    var floored = Math.floor(answer);
+    var rounded = Math.round(answer);
+    
+    console.log("Floored: ", floored);
+    console.log("Rounded: ", rounded);
+    
+    rl.close();
+});
---
+++ JavaScript-Basics-November2014/02.JavaScript Syntax/04.CheckIfNumberIsPrime/04.CheckIfNumberIsPrime.js
@@ -0,0 +1,30 @@
+﻿var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Input a number: ", function (answer) {
+    
+    var primeNum = isPrime(answer);
+    
+    console.log("Is it prime?: ", primeNum);
+    
+    rl.close();
+});
+
+function isPrime(answer) {
+    if (answer == 1) {
+        return false;
+    }
+    
+
+    for (var index = 2; index <= Math.sqrt(answer); index++) {
+        if (answer % index == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
