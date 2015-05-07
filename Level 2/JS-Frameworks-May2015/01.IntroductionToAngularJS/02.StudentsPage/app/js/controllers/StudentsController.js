f14b97464de5b3034da6aecdb30a51cf056eff37
mihayloff <mihayloff14@gmail.com>
2015-05-07 14:19:33 +0300

    added the initial folder with the first assignment & Lecture for the JS Frameworks course

---
+++ Level 2/JS-Frameworks-May2015/01.IntroductionToAngularJS/02.StudentsPage/app/js/controllers/StudentsController.js
@@ -0,0 +1,12 @@
+﻿app.controller("StudentsController", function ($scope) {
+    var student = {
+        "name": "Pesho",
+        "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+        "grade": 5,
+        "school": "High School of Mathematics",
+        "teacher": "Gichka Pesheva",
+    };
+
+    $scope.student = student;
+
+});
