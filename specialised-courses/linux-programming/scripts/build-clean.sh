1200df681498b03d2cc26b7646f970483f142420
mihayloff <pmihaylov95@gmail.com>
2016-01-29 08:42:29 -0500

    modified the build clean script

--- specialised-courses/linux-programming/scripts/build-clean.sh
+++ specialised-courses/linux-programming/scripts/build-clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf Makefile aclocal.m4 compile configure config.status depcomp Makefile.in *.tar.gz config.log missing install-sh autom4te.cache/
+rm -rf .deps Makefile aclocal.m4 compile configure config.status depcomp Makefile.in *.tar.gz config.log missing install-sh autom4te.cache/
